77a161c7a6644f5db28345dba1737a5763c81a4c
MohamedSohil85 <mimo_mnb@hotmail.com>
2026-04-04 02:15:05 +0200

    Initial commit

---
+++ src/main/java/com/ecommerce/analytics/CustomerSegmentationAndSalesForecatingForECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSegmentationAndSalesForecastingForECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSegmentationAndSalesForecastingForECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/analytics/CustomerSegmentationAndSalesForecatingForECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSegmentationAndSalesForecatingForECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
